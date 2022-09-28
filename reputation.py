86a9ae2064ce10268488fcfe6655aa44c1c53549
blood <backdoor@fbi.ac>
2022-09-28 12:01:10 +0400

    Create reputation.py

---
+++ reputation.py
@@ -0,0 +1,30 @@
+# ropro reputation botter by blood#1000
+
+
+import requests
+
+
+cookies = open("cookies.txt", "r").read().split("\n")
+
+target = input("User ID: ")
+
+
+
+for cookie in cookies:
+  
+  roproveritoken = "" # find out how ropros auth system works
+  roproid = "" # find out how ropros auth system works
+  userid = "" # do some roblox api shit
+  
+  r=requests.post(
+    "https://api.ropro.io/postReputation.php",
+    headers={
+      "ropro-verification": roproveritoken,
+      "ropro-id": roproid
+    },
+    json={
+      "type": "add",
+      myUser: userid,
+      theirUser: target
+    }
+  )
